6c3a156e41da512bc56f2fe8c8827ae2f277f1da
FeruzRus <44123610+FeruzRus@users.noreply.github.com>
2022-06-25 10:59:57 +0500

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,2 +1,1 @@
-Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AX4XfWiro8rNZjEYNEQZrxRZThYtTwuP8WsjdMNdIr_rzHHgzc8RPMeUZQT3J0aFH67ivA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME
+& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AX4XfWje1XvLGutN2Q6FK_weXCK2rEKsZyXvuo3MSg-rZlmOGN8sRfwZr2wo5fIgI4K8DA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME
